9089abc38d8da5d8ecfbf3adc1620138255d2d86
simcap <simcap@fastmail.com>
2015-02-27 12:31:39 +0100

    Moving checkcum to providers

--- cache/cache.go
+++ cache/cache.go
@@ -1,14 +1,9 @@
 package cache
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
-	"regexp"
 )
 
 func CreateRirCacheDir() {
@@ -24,9 +19,7 @@ func Refresh() {
 
 	for _, provider := range Providers {
 		go func(p Provider) {
-			local := localMd5For(p)
-			remote := remoteMd5For(p)
-			if local != remote || (remote == "" && local == "") {
+			if p.IsStale() {
 				refresh <- p
 			} else {
 				uptodate <- p
@@ -43,38 +36,4 @@ func Refresh() {
 			log.Printf("%s is up to date", u.Name())
 		}
 	}
-}
-
-func localMd5For(provider Provider) string {
-	content, err := ioutil.ReadAll(GetDataFile(provider.Name()))
-	if err != nil {
-		log.Fatalf("Cannot checksum local file for %s. %s", provider.Name(), err)
-	}
-	sum := md5.Sum(content)
-	log.Printf("Local md5 for %s is %x", provider.Name(), sum)
-	return fmt.Sprintf("%x", sum)
-}
-
-func remoteMd5For(provider Provider) string {
-	resp, err := http.Get(provider.Url() + ".md5")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	if status := resp.StatusCode; status != 200 {
-		log.Printf("Cannot GET md5 for %s. Call returned %d", provider.Name(), status)
-		return ""
-	}
-
-	md5Response, _ := ioutil.ReadAll(resp.Body)
-
-	matches := regexp.MustCompile("=\\s*(\\w+)\\s*$").FindSubmatch(md5Response)
-	if matches == nil {
-		log.Print("Cannot regexp match an md5")
-		return ""
-	}
-
-	log.Printf("Remote md5 for %s is %s", provider.Name(), string(matches[1]))
-	return string(matches[1])
 }
--- cache/providers.go
+++ cache/providers.go
@@ -2,10 +2,13 @@ package cache
 
 import (
 	"bytes"
+	"crypto/md5"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 type Provider interface {
@@ -29,7 +32,9 @@ func (p *DefaultProvider) Url() string {
 }
 
 func (p *DefaultProvider) IsStale() bool {
-	return false
+	local := p.localMd5()
+	remote := p.remoteMd5()
+	return (local != remote) || (remote == "" && local == "")
 }
 
 func (p *DefaultProvider) GetData() io.Reader {
@@ -47,6 +52,40 @@ func (p *DefaultProvider) GetData() io.Reader {
 	content, _ := ioutil.ReadAll(response.Body)
 
 	return bytes.NewBuffer(content)
+}
+
+func (p *DefaultProvider) localMd5() string {
+	content, err := ioutil.ReadAll(GetDataFile(p.Name()))
+	if err != nil {
+		log.Fatalf("Cannot checksum local file for %s. %s", p.Name(), err)
+	}
+	sum := md5.Sum(content)
+	log.Printf("Local md5 for %s is %x", p.Name(), sum)
+	return fmt.Sprintf("%x", sum)
+}
+
+func (p *DefaultProvider) remoteMd5() string {
+	resp, err := http.Get(p.Url() + ".md5")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if status := resp.StatusCode; status != 200 {
+		log.Printf("Cannot GET md5 for %s. Call returned %d", p.Name(), status)
+		return ""
+	}
+
+	md5Response, _ := ioutil.ReadAll(resp.Body)
+
+	matches := regexp.MustCompile("=\\s*(\\w+)\\s*$").FindSubmatch(md5Response)
+	if matches == nil {
+		log.Print("Cannot regexp match an md5")
+		return ""
+	}
+
+	log.Printf("Remote md5 for %s is %s", p.Name(), string(matches[1]))
+	return string(matches[1])
 }
 
 var (
